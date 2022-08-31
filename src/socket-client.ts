984def57f823fcd380cadce9523c6bd12ed57eb4
jamilmendez <mdztnr99@gmail.com>
2022-08-30 20:19:31 -0500

    prod url ws agregado

--- src/socket-client.ts
+++ src/socket-client.ts
@@ -3,7 +3,7 @@ import { Manager, Socket } from "socket.io-client";
 let socket: Socket;
 
 export const connectToServer = (token: string) => {
-  const manager = new Manager("http://localhost:3000/socket.io/socket.io.js",{
+  const manager = new Manager("https://teslo-nest-jamdev.herokuapp.com/socket.io/socket.io.js",{
     extraHeaders: {
         authentication: token,
     }
